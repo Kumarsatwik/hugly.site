5c45540b8fa2a5e2ad011b5808fb2b6e8adaa458
kumarsatwik <kumarsatwik6@gmail.com>
2025-07-06 23:46:58 +0530

    feat: integrate tRPC for API communication
    - Added tRPC client, server, and router configurations.
    - Implemented a basic "hello" procedure for testing.
    - Integrated tRPC with Next.js API routes.
    - Added a client-side component to consume the tRPC API.
    - Configured React Query for data fetching and caching.
    - Updated package.json with necessary dependencies.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { prisma } from "@/lib/db";
+import { getQueryClient } from "@/trpc/server";
+import { trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
+
 const Page = async () => {
-  
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.hello.queryOptions({ text: "text prefetch" })
+  );
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Users</h1>
-      <ul>
-        
-      </ul>
-      <Button>Click me</Button>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
